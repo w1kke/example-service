4c845796ae314f18b886bd62dcf42f75a40601d0
Robin Lehmann <4robinlehmann@gmail.com>
2020-01-15 09:28:23 +0700

    added ocean squid examples

--- service/__init__.py
+++ service/__init__.py
@@ -1,5 +1,4 @@
-registry = {
-    "example_service": {
-        "grpc": 7003,
-    },
-}
+#  Copyright 2018 Ocean Protocol Foundation
+#  SPDX-License-Identifier: Apache-2.0
+
+from .example_config import ExampleConfig
